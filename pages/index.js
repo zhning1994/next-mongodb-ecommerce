7a1b8e7f6b2eb683412ded31350817a91bc1698f
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-07-07 20:22:50 +0800

    get product data from utils, eact product will return a product card

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 import Layout from '../components/Layout';
+import data from '../utils/data';
+import ProductItem from '../components/ProductItem';
 
 export default function Home() {
-  return <Layout>Home Page</Layout>;
+  return (
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
 }
