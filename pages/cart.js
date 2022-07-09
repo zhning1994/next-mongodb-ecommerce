5e425cac166658acec0b2fd3187e8c1a3e02f331
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-07-09 23:37:00 +0800

    using dynamic rendering so that won't have hydration issue as cart number is not same at client side rendering compare to server side rendering

--- pages/cart.js
+++ pages/cart.js
@@ -5,6 +5,7 @@ import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
 import { XCircleIcon } from '@heroicons/react/outline';
 import { useRouter } from 'next/router';
+import dynamic from 'next/dynamic';
 
 const CartScreen = () => {
   const router = useRouter();
@@ -104,4 +105,4 @@ const CartScreen = () => {
   );
 };
 
-export default CartScreen;
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
