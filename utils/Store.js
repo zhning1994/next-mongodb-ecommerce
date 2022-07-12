c5b325db3b9be7e0efeda43d57bd0de57e903ca6
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-07-12 23:41:14 +0800

    save latest shipping address

--- utils/Store.js
+++ utils/Store.js
@@ -6,7 +6,7 @@ export const Store = createContext();
 const initialState = {
   cart: Cookies.get('cart')
     ? JSON.parse(Cookies.get('cart'))
-    : { cartItems: [] },
+    : { cartItems: [], shippingAddress: {}, paymentMethod: '' },
 };
 
 function reducer(state, action) {
@@ -36,6 +36,19 @@ function reducer(state, action) {
         ...state,
         cart: {
           cartItems: [],
+          shippingAddress: { location: {} },
+          paymentMethod: '',
+        },
+      };
+    case 'SAVE_SHIPPING_ADDRESS':
+      return {
+        ...state,
+        cart: {
+          ...state.cart,
+          shippingAddress: {
+            ...state.cart.shippingAddress,
+            ...action.payload,
+          },
         },
       };
     default:
