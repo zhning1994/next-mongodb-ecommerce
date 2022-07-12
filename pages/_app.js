174399e17c99640428c9d9b03f1559bc97a550a8
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-07-12 23:59:53 +0800

    only login user able to view shipping page, else push them to unauthorized page

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,37 @@
 import '../styles/globals.css';
 import { StoreProvider } from '../utils/Store';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
       <StoreProvider>
-        <Component {...pageProps} />
+        {Component.auth ? (
+          <Auth adminOnly={Component.auth.adminOnly}>
+            <Component {...pageProps} />
+          </Auth>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </StoreProvider>
     </SessionProvider>
   );
+}
+
+function Auth({ children, adminOnly }) {
+  const router = useRouter();
+  const { status, data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      router.push('/unauthorized?message=login required');
+    },
+  });
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  return children;
 }
 
 export default MyApp;
