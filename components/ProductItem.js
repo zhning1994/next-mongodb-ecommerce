e1b3a7c038814ca02c06b6fdda12cf5c655398b4
Low Zhen Ning <60384726+zhning1994@users.noreply.github.com>
2022-07-13 23:50:45 +0800

    get all products from the server, for each product item will apply addToCart handler, will toast out error if quantity more than stock, else toast successfully add to cart

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-const ProductItem = ({ product }) => {
+const ProductItem = ({ product, addToCartHandler }) => {
   return (
     <div className="card">
       <Link href={`/product/${product.slug}`}>
@@ -23,7 +23,10 @@ const ProductItem = ({ product }) => {
         </Link>
         <p className="mb-2">{product.brand}</p>
         <p>RM {product.price}</p>
-        <button className="primary-button" type="button">
+        <button
+          className="primary-button"
+          type="button"
+          onClick={() => addToCartHandler(product)}>
           Add to cart
         </button>
       </div>
